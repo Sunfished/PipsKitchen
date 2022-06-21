3058cea3c49c5bf83a3c0a00218abd6d640d14c4
Anders Hoffman <pccompositor@aim.com>
2022-06-20 19:34:50 -0500

    update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (86fe019d)";
+Config.version = "0.11.2 (df6fc64b)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',
