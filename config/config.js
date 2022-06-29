42af60cc5527cbd4c33f8f62959b05a1d8906bd1
Yoshiblaze <rosefias@gmail.com>
2022-06-29 03:22:00 -0400

    .

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (74487745)";
+Config.version = "0.11.2 (e643771f)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',
