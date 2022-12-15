20600e3d7b5b1bdc9ce3295d436a404e8dc90002
scoopapa <pccompositor@aim.com>
2022-12-14 21:10:11 -0600

    Revert "Gen 9 Update"
    This reverts commit 3ebca3c7465fb52eafae7690b69bb6c9b99e5540.

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (de1cd3e7)";
+Config.version = "0.11.2 (6c946211)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',
